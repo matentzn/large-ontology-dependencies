ad903a4785db30b06bf675ab5969a4623302628c
matentzn <nicolas.matentzoglu@gmail.com>
2019-02-21 10:26:34 +0000

    Update 20 Feb

--- package_imports.sh
+++ package_imports.sh
@@ -3,13 +3,14 @@ OBO=http://purl.obolibrary.org/obo
 ROBOT=robot
 
 for ontology in pr ncbitaxon chebi go; do
-  rm $ontology.obo
+  rm -f $ontology.obo
   wget --no-check-certificate $OBO"/"$ontology.obo -O $ontology.obo
   robot merge --input $ontology.obo --output $ontology.tmp.obo && mv $ontology.tmp.obo $ontology.obo
-  rm $ontology.obo.gz
+  rm -f $ontology.obo.gz
   gzip $ontology.obo 
+  rm -f $ontology.owl
   wget --no-check-certificate $OBO"/"$ontology.owl -O $ontology.owl
-  robot merge --input $ontology.obo --output $ontology.tmp.owl && mv $ontology.tmp.owl $ontology.owl
-  rm $ontology.owl.gz
+  robot merge --input $ontology.owl --output $ontology.tmp.owl && mv $ontology.tmp.owl $ontology.owl
+  rm -f $ontology.owl.gz
   gzip $ontology.owl
 done 
